6756fca596742fcd9b7a2ccdc825118a87d0c825
Bence Dányi <Bence.Danyi@ibm.com>
2020-10-16 12:49:30 +0200

    fix: use `ibmcloud-api-key` in prod deployments

--- scripts/setup_deploy.sh
+++ scripts/setup_deploy.sh
@@ -10,14 +10,14 @@ export REGISTRY_URL
 export IMAGE_PULL_SECRET_NAME
 export IMAGE
 
-IBMCLOUD_API_KEY="$(cat /config/api-key)" # pragma: allowlist secret
-
-if [ -f /config/dev-region ]; then
-  IBMCLOUD_IKS_REGION="$(cat /config/dev-region)"
-  IBMCLOUD_IKS_CLUSTER_NAMESPACE="$(cat /config/dev-cluster-namespace)"
+if [ -f /config/api-key ]; then
+  IBMCLOUD_API_KEY="$(cat /config/api-key)" # pragma: allowlist secret
+else
+  IBMCLOUD_API_KEY="$(cat /config/ibmcloud-api-key)" # pragma: allowlist secret
 fi
-IBMCLOUD_IKS_REGION=$(echo "${IBMCLOUD_IKS_REGION}" | awk -F ":" '{print $NF}')
 
+IBMCLOUD_IKS_REGION="$(cat /config/dev-region | awk -F ":" '{print $NF}')"
+IBMCLOUD_IKS_CLUSTER_NAMESPACE="$(cat /config/dev-cluster-namespace)"
 IBMCLOUD_IKS_CLUSTER_NAME="$(cat /config/cluster-name)"
 REGISTRY_URL="$(cat /config/image | awk -F/ '{print $1}')"
 IMAGE="$REGISTRY_URL/$(cat /config/image | awk -F "/|@" '{print $2"/"$3"@"$4}')"
