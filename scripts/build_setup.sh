6380e03c426efa4f402e016a4e16953f6a7cbc48
Szabolcs Szabolcsi-Tóth <szabolcsit@hu.ibm.com>
2020-11-30 14:10:16 +0100

    feat: create cr namespace before build to icr

--- scripts/build_setup.sh
+++ scripts/build_setup.sh
@@ -51,6 +51,20 @@ else
   ICR_REGISTRY_REGION="$(get-icr-region "$(cat /config/registry-region)")"
   IMAGE="$ICR_REGISTRY_REGION.icr.io/$ICR_REGISTRY_NAMESPACE/$IMAGE_NAME:$IMAGE_TAG"
   docker login -u iamapikey --password-stdin "$ICR_REGISTRY_REGION.icr.io" < /config/api-key
+  
+  # Create the namespace if needed to ensure the push will be can be successfull
+  echo "Checking registry namespace: ${ICR_REGISTRY_NAMESPACE}"
+  IBM_LOGIN_REGISTRY_REGION=$(cat /config/registry-region | awk -F: '{print $3}')
+  ibmcloud login --apikey @/config/api-key -r $IBM_LOGIN_REGISTRY_REGION
+  NS=$( ibmcloud cr namespaces | grep ${ICR_REGISTRY_NAMESPACE} ||: )
+  
+  if [ -z "${NS}" ]; then
+      echo "Registry namespace ${ICR_REGISTRY_NAMESPACE} not found"
+      ibmcloud cr namespace-add ${ICR_REGISTRY_NAMESPACE}
+      echo "Registry namespace ${ICR_REGISTRY_NAMESPACE} created."
+  else
+      echo "Registry namespace ${ICR_REGISTRY_NAMESPACE} found."
+  fi
 fi
 
 DOCKER_BUILD_ARGS="-t $IMAGE"
